1493cf20ea098528c38c23efa6f121b469c9bb1e
Jared Mayer <jmayer@learnvest.com>
2016-07-10 16:52:29 -0400

    added searchbar to import. working with Hi. Changing to searchbar soon

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyCaJIqVBhaj2uRtCKQGLUToB0p-jK9mAYk';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+	return <div>Hi!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,11 @@ module.exports = {
   },
   module: {
     loaders: [{
+      test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'babel',
       query: {
-        presets: ['react', 'es2015', 'stage-1']
+        presets: ['react', 'es2015']
       }
     }]
   },
